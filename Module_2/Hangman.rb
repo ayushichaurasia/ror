8754aaabaf928c4fda60a2c0a496905986899922
Ayushi <ayushi1509ac@gmail.com>
2025-03-04 12:18:28 +0530

    Modified code to ask user play again, fixed intendation as suggested

--- Module_2/Hangman.rb
+++ Module_2/Hangman.rb
@@ -1,28 +1,32 @@
 class Hangman
+  ATTEMPTS = 6
+
   def initialize
     @words = ["People", "Schedule", "Success", "Positivity", "Mischievous"]
-    @attempts = 6
+    @attempts = ATTEMPTS
     @word = @words.sample.downcase.chars
     @guessed_letters = []
-    @display_word = Array.new(@word.length,"_")
+    @display_word = Array.new(@word.length, "_")
   end
 
   def play
-    puts "Welcome to Hnagman Game!"
+    puts "Welcome to Hangman Game!"
+    
     while @attempts > 0 && @display_word.include?("_")
-      puts "\nWord: " +@display_word.join(" ")
-      puts "\nTotal letters: #{@word.length}"
+      puts "\nWord: " + @display_word.join(" ")
+      puts "Total letters: #{@word.length}"
       puts "Attempts left: #{@attempts}"
-      puts "Your guessed letters: #{@guessed_letters.join(", ")}"
-      puts "Please enter a letter: "
+      puts "Your guessed letters: " + @guessed_letters.join(", ")
+      print "Please enter a letter: "
       guess = gets.chomp.downcase
 
       if valid_guess?(guess)
         guess_check(guess)
       else
-        puts "Invalid input. Please enter one letter at a time." 
+        puts "Invalid input. Please enter one letter at a time."
       end
     end
+
     game_over
   end
 
@@ -34,25 +38,38 @@ class Hangman
 
   def guess_check(guess)
     @guessed_letters << guess
+
     if @word.include?(guess)
-      @word.each_with_index {|char, index| @display_word[index]=char if char == guess}
-      if @display_word.count("_") > 0
-      puts "Good going, Guess the next letter!"
-      end
+      @word.each_with_index { |char, index| @display_word[index] = char if char == guess }
+      puts "Good going! Guess the next letter!" if @display_word.include?("_")
+
     else
+
       @attempts -= 1
-      puts "Uh oh, got the wrong one! No Worries! Try again."
+      puts "Uh oh, got the wrong one! No worries! Try again."
     end
   end
 
   def game_over
     if @display_word.include?("_")
-    puts "\nGame Over! The word was '#{@word.join}'" 
-  else
-    puts "\nWord: " +@display_word.join(" ")
-    puts "\nCongrats Champ! You guessed the word right: '#{@word.join}'"
+      puts "\nGame Over! The word was '#{@word.join}'"
+    else
+      puts "\nWord: " + @display_word.join(" ")
+      puts "\nCongrats Champ! You guessed the word right: '#{@word.join}'"
+    end
+
+    restart_game?
   end
-end
+
+  def restart_game?
+    print "\nDo you want to play Hangman again? (yes/no): "
+    choice = gets.chomp.downcase
+    if choice == "yes"
+      Hangman.new.play
+    else
+      puts "Thanks for playing!"
+    end
+  end
 end
 
 Hangman.new.play
