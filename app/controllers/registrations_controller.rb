731a5499e74be3dcdb870f4ad086893b6bd7969d
Ayushi <ayushi1509ac@gmail.com>
2025-03-24 12:57:40 +0530

    modifide welcome message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
     def create
       super do |resource|
         if resource.persisted?
-          flash[:notice] = "Welcome, #{resource.name}! 🎉"
+          flash[:notice] = "Welcome!, #{resource.name}! 🎉"
         end
       end
     end
